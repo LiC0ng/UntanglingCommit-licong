7c575dd4c494b3f0326533b22bbe3c7c42b5b1af
LiC0ng <a1040484@gmail.com>
2021-03-06 11:47:57 +0800

    (dataset_handle): avoid blank cow in dataset

--- dataset_handle/create_dataset.py
+++ dataset_handle/create_dataset.py
@@ -1,94 +1,51 @@
 from argparse import ArgumentParser
 import random
 
+def create_dataset(index_dir, dest_dir, repo):
+    type_list = ["ins", "ino", "its", "ito"]
+    for type in type_list:
+        train_true_file = index_dir + '/train/true/' + type + '.txt'
+        train_false_file = index_dir + '/train/false/' + type + '.txt'
+        with open(train_true_file, 'r') as true_file:
+            positive_data = true_file.read().replace('\n\n', '\n').split('\n')
+        with open(train_false_file, 'r') as false_file:
+            negative_data = false_file.read().replace('\n\n', '\n').split('\n')
+        minLen = min(len(positive_data), len(negative_data))
+        random.shuffle(positive_data)
+        random.shuffle(negative_data)
+        output_path = dest_dir + '/train/' + type + '.txt'
+        with open(output_path, 'a') as output_file:
+            for i in range(minLen):
+                output_file.writelines(positive_data[i] + '\n')
+                output_file.writelines(negative_data[i] + '\n')
 
-def ReadFileDatas(index_path, isCommit, isTrain):
-    FileNamelist = []
-    file = open(index_path, 'r')
-    for line in file:
-        if line == "" or line == " ":
-            continue
-        if isCommit:
-            FileNamelist.append(line.strip('\n'))
-        else:
-            FileNamelist.append(line)
-    if isCommit and isTrain:
-        print('len ( Train Commit ) = ', len(FileNamelist))
-    elif isCommit and not isTrain:
-        print('len ( Test Commit ) = ', len(FileNamelist))
-    elif not isCommit:
-        print('len ( Data ) = ', len(FileNamelist))
-    file.close()
-    return FileNamelist
-
-
-def WriteDatasToFile(data_path, repo):
-    # create train dataset
-    train_data = open(data_path + '/traindata.txt', mode='a')
-    truelist = []
-    falselist = []
-    for commit in trueTrainCommitList:
-        for data in untangled_list:
-            if(data.find(commit) >= 0):
-                truelist.append(data)
-
-    for commit in falseTrainCommitList:
-        for data in tangled_list:
-            commitPair = commit.split(" ")
-            if(data.find(commitPair[0]) >= 0 and data.find(commitPair[1]) >= 0):
-                falselist.append(data)
-    minLen = min(len(truelist), len(falselist))
-    random.shuffle(truelist)
-    random.shuffle(falselist)
-    for i in range(minLen):
-        train_data.writelines(truelist[i])
-        train_data.writelines(falselist[i])
-    train_data.close()
-
-    # create test dataset
-    test_data = open(data_path + '/totaltest.txt', mode='a')
-    test_project_data = open(data_path + '/' + repo + '_test.txt', mode='a')
-    for commit in trueTestCommitList:
-        for data in untangled_list:
-            if(data.find(commit) >= 0):
-                test_data.writelines(data)
-                test_project_data.writelines(data)
-
-    for commit in falseTestCommitList:
-        for data in tangled_list:
-            commitPair = commit.split(" ")
-            if(data.find(commitPair[0]) >= 0 and data.find(commitPair[1]) >= 0):
-                test_data.writelines(data)
-                test_project_data.writelines(data)
-    test_data.close()
-    test_project_data.close()
-
-    # create cluster dataset
-    for commit in falseTestCommitList:
-        commitPair = commit.split(" ")
-        composite_commit = open(data_path + '/cluster/' + repo + '/' + commitPair[0] + '_' + commitPair[1] + '.txt', mode='w')
-        for data in tangled_list:
-            if(data.find(commitPair[0]) >= 0 and data.find(commitPair[1]) >= 0):
-                composite_commit.writelines(data)
-        for data in untangled_list:
-            if(data.find(commitPair[0]) >= 0 or data.find(commitPair[1]) >= 0):
-                composite_commit.writelines(data)
-        composite_commit.close()
+    for type in type_list:
+        test_true_file = index_dir + '/test/true/' + type + '.txt'
+        test_false_file = index_dir + '/test/false/' + type + '.txt'
+        with open(test_true_file, 'r') as true_file:
+            positive_data = true_file.read().split('\n')
+        with open(test_false_file, 'r') as false_file:
+            negative_data = false_file.read().split('\n')
+        output_path = dest_dir + '/test/all/' + type + '.txt'
+        output_path_with_project = dest_dir + '/test/' + repo + '/' + type + '.txt'
+        with open(output_path, 'a') as output_file, \
+                open(output_path_with_project, 'a') as project_output_file:
+            for data in positive_data:
+                if data == "" or data == " " or data == "\n":
+                    continue
+                output_file.writelines(data + '\n')
+                project_output_file.writelines(data + '\n')
+            for data in negative_data:
+                if data == "" or data == " " or data == "\n":
+                    continue
+                output_file.writelines(data + '\n')
+                project_output_file.writelines(data + '\n')
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--source_dir", dest="source_dir", required=True)
+    parser.add_argument("--index_dir", dest="index_dir", required=True)
     parser.add_argument("--dest_dir", dest="dest_dir", required=True)
     parser.add_argument("--repo", dest="repo", required=True)
-    parser.add_argument("--commits_dir", dest="commits_dir", required=True)
     args = parser.parse_args()
-    trueTestCommitList = ReadFileDatas(args.commits_dir + '/test/true.csv', True, False)
-    falseTestCommitList = ReadFileDatas(args.commits_dir + '/test/false.csv', True, False)
-    trueTrainCommitList = ReadFileDatas(args.commits_dir + '/train/true.csv', True, True)
-    falseTrainCommitList = ReadFileDatas(args.commits_dir + '/train/false.csv', True, True)
-
-    tangled_list = ReadFileDatas(args.source_dir + '/tangled.txt', False, False)
-    untangled_list = ReadFileDatas(args.source_dir + '/untangled.txt', False, False)
-
-    WriteDatasToFile(args.dest_dir, args.repo)
+    create_dataset(args.index_dir, args.dest_dir, args.repo)
--- dataset_handle/create_dataset.sh
+++ dataset_handle/create_dataset.sh
@@ -5,17 +5,17 @@ PYTHON='python'
 DATA_DIR=dataset/dataset
 
 mkdir -p ${DATA_DIR}
-
-rm -r ${DATA_DIR}/*
+mkdir -p ${DATA_DIR}/test/all
 
 echo "Creating dataset"
 for repo in $(ls dataset/pre); do
   PRE_DATASET=dataset/pre/${repo}
   INDEX_DIR=${PRE_DATASET}/index
-  COMMIT_DIR=${PRE_DATASET}/commits
   mkdir -p ${DATA_DIR}/cluster/${repo}
+  mkdir -p ${DATA_DIR}/train
+  mkdir -p ${DATA_DIR}/test/${repo}
 
-  ${PYTHON} dataset_handle/create_dataset.py --source_dir ${INDEX_DIR} \
-    --dest_dir ${DATA_DIR}  --repo ${repo} --commits_dir ${COMMIT_DIR}
+  ${PYTHON} dataset_handle/create_dataset.py --index_dir ${INDEX_DIR} \
+    --dest_dir ${DATA_DIR}  --repo ${repo}
 done
 echo "Create dataset complete"
