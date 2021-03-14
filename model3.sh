be2157927fbbcd542f6c9b933bb012065cafd3af
LiC0ng <a1040484@gmail.com>
2021-03-14 16:59:07 +0900

    (model): set scripts' python version

--- model3.sh
+++ model3.sh
@@ -1,10 +1,15 @@
+#!/usr/bin/env bash
+###########################################################
+PYTHON='python3'
+###########################################################
+
 mkdir -p dataset/cluster/3/argouml
 mkdir -p dataset/cluster/3/gwt
 mkdir -p dataset/cluster/3/jruby
 mkdir -p dataset/cluster/3/xstream
 mkdir -p dataset/cluster/3/all
 
-python ./model3_idPACE_otherPACE.py --type ins
-python ./model3_idPACE_otherPACE.py --type ino
-python ./model3_idPACE_otherPACE.py --type its
-python ./model3_idPACE_otherPACE.py --type ito
+${PYTHON} ./model3_idPACE_otherPACE.py --type ins
+${PYTHON} ./model3_idPACE_otherPACE.py --type ino
+${PYTHON} ./model3_idPACE_otherPACE.py --type its
+${PYTHON} ./model3_idPACE_otherPACE.py --type ito
--- model4.sh
+++ model4.sh
@@ -1,10 +1,15 @@
+#!/usr/bin/env bash
+###########################################################
+PYTHON='python3'
+###########################################################
+
 mkdir -p dataset/cluster/4/argouml
 mkdir -p dataset/cluster/4/gwt
 mkdir -p dataset/cluster/4/jruby
 mkdir -p dataset/cluster/4/xstream
 mkdir -p dataset/cluster/4/all
 
-python ./model4_idPACE_otherN2N.py --type ins
-python ./model4_idPACE_otherN2N.py --type ino
-python ./model4_idPACE_otherN2N.py --type its
-python ./model4_idPACE_otherN2N.py --type ito
+${PYTHON} ./model4_idPACE_otherN2N.py --type ins
+${PYTHON} ./model4_idPACE_otherN2N.py --type ino
+${PYTHON} ./model4_idPACE_otherN2N.py --type its
+${PYTHON} ./model4_idPACE_otherN2N.py --type ito
